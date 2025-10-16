425155e496c0befbf55a17bcc152e7f1a4d276bc
lemeinannette <emaillemeinannette@gmail.com>
2025-10-16 15:37:34 +0300

    update

--- src/components/admin/AdminLogin.jsx
+++ src/components/admin/AdminLogin.jsx
@@ -1,7 +1,7 @@
 // src/components/admin/AdminLogin.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Dashboard.css';
+import './AdminLogin.css';
 
 const AdminLogin = ({ onLogin }) => {
   const [formData, setFormData] = useState({
