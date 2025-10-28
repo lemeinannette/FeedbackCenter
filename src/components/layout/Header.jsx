0b0ff34d3734142d6e10bdc23d9fc956fc385b42
lemeinannette <emaillemeinannette@gmail.com>
2025-10-28 12:48:59 +0300

    logo

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -35,9 +35,10 @@ const Header = () => {
           <Link to="/" className="logo">
             <div className="logo-icon">
               <div className="logo-inner">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M20 6L9 17L4 12" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                <div className="logo-fc">
+                  <span className="logo-f">F</span>
+                  <span className="logo-c">c</span>
+                </div>
               </div>
             </div>
             <div className="logo-text">
